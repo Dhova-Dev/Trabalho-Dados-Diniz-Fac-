cc2ee9b8640c818da9f85a2c531b347fc5a80904
Dhova-Dev <141073912+Dhova-Dev@users.noreply.github.com>
2024-06-07 14:26:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('dice-roll').addEventListener('click', function() {
+    var dice = document.getElementById('dice-selector').value;
+    var result = Math.floor(Math.random() * dice) + 1;
+    document.getElementById('result').innerText = 'Resultado : ' + result;
+});
+
+const themes = ['light', 'dark', 'high-contrast'];
+const themeEmojis = ['🌞', '🌜', '⚫'];
+let currentThemeIndex = 0;
+
+document.getElementById('theme').addEventListener('click', function() {
+    currentThemeIndex = (currentThemeIndex + 1) % themes.length;
+    document.body.className = themes[currentThemeIndex];
+    this.innerText = themeEmojis[currentThemeIndex] + ' Mudar Tema';
+});
